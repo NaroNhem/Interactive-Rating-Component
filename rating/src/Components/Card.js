9eeac3c5234b60dc85d51d803720d2321cf297f1
Justin <n.justinaro@gmail.com>
2023-03-01 17:44:05 -0500

    Basic rendering

---
+++ rating/src/Components/Card.js
@@ -0,0 +1,20 @@
+export default function Card() {
+    return (
+<div>
+      <div>
+        <img src="images/icon-star.svg" alt="icon-star"></img>
+      </div>
+      <h3>How did we do?</h3>
+      <p>Please let us kknow how we did with your support request. All feedback is appreciate to help us improve our offering!</p>
+      <button>Sumit</button>
+      <div style={{ display: "flex" }}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </div>
+        <button>Submit</button>
+    </div>
+    )
+}
